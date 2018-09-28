5623d70c7dde7839af9c06db538a69403ca6d93b
Frednicht <Frednicht@TIJLZ>
2018-09-28 09:56:38 +0200

    Joooow

---
+++ Webapp Week1 les 2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Webapp_Week1_les_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
